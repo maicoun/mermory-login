88b9887284292f3231097555bf30771845b16b5a
Maicon Goncalvesq <maicongoncalvesq@MacBook-Air-de-Maicon.local>
2025-01-29 09:05:31 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginForm from './components/ LoginForm';
+import LoginForm from './components/LoginForm';
 
 ReactDOM.render(
   <React.StrictMode>
